3f87e01a0a910f30ed182e0117bf03618751d7e0
FrcTeam4955 <lakesiderobotics10@gmail.com>
2017-02-11 12:12:28 -0500

    Added method to WallSensor.java

--- src/org/usfirst/frc/team4955/robot/commands/drive/WallSensor.java
+++ src/org/usfirst/frc/team4955/robot/commands/drive/WallSensor.java
@@ -1,5 +1,15 @@
 package org.usfirst.frc.team4955.robot.commands.drive;
 
-public class WallSensor {
+import edu.wpi.first.wpilibj.command.Command;
 
+public class WallSensor extends Command{
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+	
 }
